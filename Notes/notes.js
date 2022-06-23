77fcb8a8f6fcbff4c619e9bb2a10a2a19bbe4437
Matt Heine <102334843+mattheine@users.noreply.github.com>
2022-06-22 23:50:07 -0600

    updated notes

--- Notes/notes.js
+++ Notes/notes.js
@@ -70,7 +70,7 @@ class DOMManager {
   static render(houses) {
     this.houses = houses;
     $("#app").empty();
-    //forloop to go over houses and rerenderDOM
+    //forloop to go over each house and rerenderDOM
     for (let house of houses) {
       $("#app").append(
         `<div id="${house._id}" class="card">
@@ -78,11 +78,28 @@ class DOMManager {
                  <h2>${house.name}</h2>
                  <button class="btn btn-danger" onclick = "DOMManager.deleteHouse('${house._id}')" >Delete </button>
             </div>
+            <div class="card-body">
+              <div class="card">
+                <div class="row">
+                  <div class="col-sm">
+                  <input type="text" id="${house._id}-Room-name" class="form-control"" placeholder="Room Name">
+                  </div>
+                  <div class="col-sm">
+                  <input type="text" id="${house._id}-room-area" class="form-control"" placeholder="Room Area">
+                  </div>
+              </div>
+              <button id="${house._id}-new-room" onclick= "DOMManager.addRoom('${house._id}')" class = "btn btn-primary form-control">Add</button>
+            </div><br>
          </div>`
+         //nested loop to render each room inside each house
       );
     }
     console.log(houses);
   }
 }
+
+/*static deleteHouse(id){
+  return 
+}*/
 
 DOMManager.getAllHouses();
